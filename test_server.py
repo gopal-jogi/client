6008e70fcc265db177344fa9e2aa17711dc85256
Gopal Dnyaneshwar Jogi <84375687+gopal-jogi@users.noreply.github.com>
2023-12-08 20:30:10 +0530

    Update test_server.py

--- test_server.py
+++ test_server.py
@@ -3,60 +3,51 @@ import http.client
 
 class TestSimpleServer(unittest.TestCase):
     def test_server_response(self):
-        # Set the server address and port
+        # address and port
         server_address = ('localhost', 9000)
 
-        # Create an HTTP connection to the server
+        #  HTTP connection to the server
         connection = http.client.HTTPConnection(*server_address)
 
         try:
-            # Send a GET request to the server with a data parameter
+            # Send a GET request 
             connection.request('GET', '/?data=TestValue')
 
-            # Get the response from the server
             response = connection.getresponse()
             
-
-            # Check that the response status code is 200 OK
             self.assertEqual(response.status, 200)
 
-            # Read and decode the response content
             content = response.read().decode('utf-8')
 
-            # Check that the response contains the expected data
             self.assertIn('Received data: TestValue', content)
 
         finally:
-            # Close the connection
+            # Close 
             connection.close()
 
     def test_post_request(self):
-        # Set the server address and port
+
         server_address = ('localhost', 9000)
 
-        # Create an HTTP connection to the server
         connection = http.client.HTTPConnection(*server_address)
 
         try:
-            # Send a POST request to the server with a data parameter
             data = 'data=TestPostValue'
             headers = {'Content-type': 'application/x-www-form-urlencoded'}
             connection.request('POST', '/', data, headers)
 
-            # Get the response from the server
             response = connection.getresponse()
 
-            # Check that the response status code is 200 OK
+        
             self.assertEqual(response.status, 200)
 
-            # Read and decode the response content
+        
             content = response.read().decode('utf-8')
 
-            # Check that the response indicates successful reception of POST data
+            
             self.assertIn("POST data received and saved to file ", content)
 
         finally:
-            # Close the connection
             connection.close()
 
 if __name__ == '__main__':
